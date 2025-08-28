ca34a474bd6c7b85b8696f651694970fe05163b9
Taylor <78652522+Taylor-eOS@users.noreply.github.com>
2025-08-28 21:42:24 +0200

    Decoding 8-byte packets that pass CRC

--- receiver.py
+++ receiver.py
@@ -1,70 +1,74 @@
-import machine
-import time
-cs = machine.Pin(17, machine.Pin.OUT)
-spi = machine.SPI(0, baudrate=500000, polarity=0, phase=0, sck=machine.Pin(18), mosi=machine.Pin(19), miso=machine.Pin(16))
-gdo0 = machine.Pin(15, machine.Pin.IN)
+import machine,time
+cs=machine.Pin(17,machine.Pin.OUT)
+spi=machine.SPI(0,baudrate=500000,polarity=0,phase=0,sck=machine.Pin(18),mosi=machine.Pin(19),miso=machine.Pin(16))
+gdo0=machine.Pin(15,machine.Pin.IN)
 def strobe(cmd):
     cs.value(0)
-    buf = bytearray([cmd])
-    spi.write_readinto(buf, buf)
+    b=bytearray([cmd])
+    spi.write_readinto(b,b)
     cs.value(1)
-    return buf[0]
-def write_reg(addr, value):
+    return b[0]
+def read_status_once(addr):
+    out=bytearray([addr|0xC0,0x00])
+    inp=bytearray(2)
     cs.value(0)
-    spi.write(bytearray([addr, value]))
+    spi.write_readinto(out,inp)
     cs.value(1)
-def read_status(addr):
-    buf = bytearray([addr | 0x80, 0x00])
-    res = bytearray(2)
+    return inp[1]
+def read_status_stable(addr,tries=8):
+    a=read_status_once(addr)
+    b=read_status_once(addr)
+    while a!=b and tries>0:
+        a=b
+        b=read_status_once(addr)
+        tries-=1
+    return b
+def write_reg(a,v):
     cs.value(0)
-    spi.write_readinto(buf, res)
+    spi.write(bytearray([a,v]))
     cs.value(1)
-    return res[1]
-def read_reg_safe(addr):
-    v1 = read_status(addr)
-    v2 = read_status(addr)
-    while v1 != v2:
-        v1 = v2
-        v2 = read_status(addr)
-    return v1
+def rssi_to_dbm(r):
+    if r>=128:
+        r=r-256
+    return (r/2.0)-72.0
 def init():
     cs.value(1)
-    time.sleep(0.01)
+    time.sleep_ms(20)
     strobe(0x30)
-    time.sleep(0.02)
-    write_reg(0x02, 0x06)
-    write_reg(0x04, 0xD3)
-    write_reg(0x05, 0x91)
-    write_reg(0x06, 8)
-    write_reg(0x07, 0x68)
-    write_reg(0x08, 0x04)
-    write_reg(0x0D, 0x21)
-    write_reg(0x0E, 0x62)
-    write_reg(0x0F, 0x76)
-    write_reg(0x10, 0x8C)
-    write_reg(0x11, 0x22)
-    write_reg(0x12, 0x02)
-    write_reg(0x13, 0x22)
-    write_reg(0x15, 0x47)
-    write_reg(0x17, 0x30)
-    write_reg(0x18, 0x18)
+    time.sleep_ms(20)
+    write_reg(0x02,0x06)
+    write_reg(0x04,0xD3)
+    write_reg(0x05,0x91)
+    write_reg(0x06,8)
+    write_reg(0x07,0x68)
+    write_reg(0x08,0x04)
+    write_reg(0x0D,0x21)
+    write_reg(0x0E,0x62)
+    write_reg(0x0F,0x76)
+    write_reg(0x10,0x8C)
+    write_reg(0x11,0x22)
+    write_reg(0x12,0x02)
+    write_reg(0x13,0x22)
+    write_reg(0x15,0x47)
+    write_reg(0x17,0x30)
+    write_reg(0x18,0x18)
+    strobe(0x36)
     strobe(0x3A)
     strobe(0x3B)
     strobe(0x34)
-def run():
-    strobe(0x34)
-    while True:
-        rxbytes = read_reg_safe(0x3B) & 0x7F
-        pktstatus = read_status(0x38)
-        marc = read_status(0x35) & 0x1F
-        rssi = read_status(0x34)
-        lqi = read_status(0x33)
-        if rxbytes>0:
-            print("RXBYTES", rxbytes, "PKTSTATUS", hex(pktstatus), "MARC", hex(marc), "RSSI", rssi, "LQI", lqi, "GDO0", gdo0.value())
-            strobe(0x3A)
-            strobe(0x3B)
-            strobe(0x34)
-        time.sleep(0.05)
 init()
-run()
+while True:
+    rx=read_status_stable(0x3B)&0x7F
+    pktst=read_status_stable(0x38)
+    marc=read_status_stable(0x35)&0x1F
+    r=read_status_stable(0x34)
+    l=read_status_stable(0x33)
+    crc_ok=(l>>7)&1
+    if rx>0:
+        print("RXBYTES",rx,"PKTSTATUS",hex(pktst),"MARC",hex(marc),"RSSI_dbm",rssi_to_dbm(r),"LQI",l&0x7F,"CRC_OK",crc_ok,"GDO0",gdo0.value())
+        strobe(0x36)
+        strobe(0x3A)
+        strobe(0x3B)
+        strobe(0x34)
+    time.sleep_ms(50)
 
--- transmitter.py
+++ transmitter.py
@@ -1,82 +1,57 @@
-import machine
-import time
-cs = machine.Pin(17, machine.Pin.OUT)
-spi = machine.SPI(0, baudrate=500000, polarity=0, phase=0, sck=machine.Pin(18), mosi=machine.Pin(19), miso=machine.Pin(16))
-gdo0 = machine.Pin(15, machine.Pin.IN)
+import machine,time
+cs=machine.Pin(17,machine.Pin.OUT)
+spi=machine.SPI(0,baudrate=500000,polarity=0,phase=0,sck=machine.Pin(18),mosi=machine.Pin(19),miso=machine.Pin(16))
 def strobe(cmd):
     cs.value(0)
-    buf = bytearray([cmd])
-    spi.write_readinto(buf, buf)
+    b=bytearray([cmd])
+    spi.write_readinto(b,b)
     cs.value(1)
-    return buf[0]
-def write_reg(addr, value):
+    return b[0]
+def write_reg(a,v):
     cs.value(0)
-    spi.write(bytearray([addr, value]))
+    spi.write(bytearray([a,v]))
     cs.value(1)
-def write_burst(addr, data):
+def write_burst(a,data):
     cs.value(0)
-    spi.write(bytearray([addr | 0x40]) + data)
+    spi.write(bytearray([a|0x40])+data)
     cs.value(1)
-def read_status(addr):
-    buf = bytearray([addr | 0x80, 0x00])
-    res = bytearray(2)
-    cs.value(0)
-    spi.write_readinto(buf, res)
-    cs.value(1)
-    return res[1]
 def init():
     cs.value(1)
-    time.sleep(0.01)
+    time.sleep_ms(20)
     strobe(0x30)
-    time.sleep(0.02)
-    write_reg(0x02, 0x06)
-    write_reg(0x04, 0xD3)
-    write_reg(0x05, 0x91)
-    write_reg(0x06, 8)
-    write_reg(0x07, 0x68)
-    write_reg(0x08, 0x04)
-    write_reg(0x0D, 0x21)
-    write_reg(0x0E, 0x62)
-    write_reg(0x0F, 0x76)
-    write_reg(0x10, 0x8C)
-    write_reg(0x11, 0x22)
-    write_reg(0x12, 0x02)
-    write_reg(0x13, 0x22)
-    write_reg(0x15, 0x47)
-    write_reg(0x17, 0x30)
-    write_reg(0x18, 0x18)
-    write_burst(0x3E, bytearray([0x60,0x00,0x00,0x00,0x00,0x00,0x00,0x00]))
-    strobe(0x36)
-    strobe(0x3B)
-    strobe(0x3A)
-def tx_once(counter):
-    pkt = f"MSG{counter:05d}".encode()
-    pkt = pkt[:8] + b'\x00'*(8 - len(pkt))
-    if not (strobe(0x36) or True):
-        pass
-    write_burst(0x3F, pkt)
-    time.sleep(0.005)
-    strobe(0x35)
-    timeout = 2000
-    while timeout>0:
-        s = strobe(0x3D)
-        marc = s & 0x1F
-        txbytes = read_status(0x3A) & 0x7F
-        if marc==0x13:
-            print("TX state")
-        if marc==0x01 and txbytes==0:
-            return True
-        timeout -= 1
-        time.sleep(0.001)
-    return False
+    time.sleep_ms(20)
+    write_reg(0x02,0x06)
+    write_reg(0x04,0xD3)
+    write_reg(0x05,0x91)
+    write_reg(0x06,8)
+    write_reg(0x07,0x68)
+    write_reg(0x08,0x04)
+    write_reg(0x0D,0x21)
+    write_reg(0x0E,0x62)
+    write_reg(0x0F,0x76)
+    write_reg(0x10,0x8C)
+    write_reg(0x11,0x22)
+    write_reg(0x12,0x02)
+    write_reg(0x13,0x22)
+    write_reg(0x15,0x47)
+    write_reg(0x17,0x30)
+    write_reg(0x18,0x18)
 init()
-i = 0
+i=0
 while True:
-    ok = tx_once(i)
-    if ok:
-        print("sent", i)
-    else:
-        print("tx fail", i)
-    i += 1
+    pkt=f"MSG{i:05d}".encode()[:8]
+    if len(pkt)<8:
+        pkt=pkt+b'\x00'*(8-len(pkt))
+    write_burst(0x3F,pkt)
+    time.sleep_ms(5)
+    strobe(0x35)
+    t0=time.ticks_ms()
+    while time.ticks_diff(time.ticks_ms(),t0)<2000:
+        s=strobe(0x3D)
+        marc=s&0x1F
+        if marc==0 and (strobe(0x3D)&0x0F)==0:
+            break
+        time.sleep_ms(2)
+    i+=1
     time.sleep(3)
 
